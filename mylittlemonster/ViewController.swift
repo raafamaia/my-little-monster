71849c0d5c58677559e6cbac06b192017968bf04
Maia <raafamaia@gmail.com>
2016-05-22 18:36:35 -0300

    Initial Commit

--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var hearImg: DragImg!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var imgArray = [UIImage]()
+        for x in 1...4 {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
